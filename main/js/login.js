e6f99b06906823b3b9e3f9c0e203ba5560b69b56
Austin <awood076@gmail.com>
2025-03-26 02:31:40 -0400

    Add login check

--- main/js/login.js
+++ main/js/login.js
@@ -1,3 +1,13 @@
-document.getElementById("loginSubmit").addEventListener("click",function(){
-    chrome.sidePanel.setOptions({path: "../html/menu.html"});
-  })
+import { getAuth, signInAnonymously } from "./../node_modules/firebase/firebase-auth.js";
+import { firebaseApp } from "./firebase-config.js";
+
+const auth = getAuth(firebaseApp);
+document.getElementById("loginSubmit").addEventListener("click", () => {
+  signInAnonymously(auth)
+    .then(() => {
+      chrome.sidePanel.setOptions({ path: "../html/menu.html" });
+    })
+    .catch((error) => {
+      console.error("Authentication failed", error);
+    });
+});
--- main/js/menu.js
+++ main/js/menu.js
@@ -1,9 +1,19 @@
-document.getElementById("searchCouponsLink").addEventListener("click",function(){
-    chrome.sidePanel.setOptions({path: "../html/sidepanel.html"});
+import { getAuth, onAuthStateChanged } from "./../node_modules/firebase/firebase-auth.js";
+import { firebaseApp } from "./firebase-config.js";
+
+const auth = getAuth(firebaseApp);
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        chrome.sidePanel.setOptions({ path: "../html/login.html" });
+    }
+});
+
+document.getElementById("searchCouponsLink").addEventListener("click", function () {
+    chrome.sidePanel.setOptions({ path: "../html/sidepanel.html" });
 })
-document.getElementById("couponsLink").addEventListener("click",function(){
-    chrome.sidePanel.setOptions({path: "../html/myCoupons.html"});
+document.getElementById("couponsLink").addEventListener("click", function () {
+    chrome.sidePanel.setOptions({ path: "../html/myCoupons.html" });
 })
-document.getElementById("logOutLink").addEventListener("click",function(){
-    chrome.sidePanel.setOptions({path: "../html/login.html"});
+document.getElementById("logOutLink").addEventListener("click", function () {
+    chrome.sidePanel.setOptions({ path: "../html/login.html" });
 })
